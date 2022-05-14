66c942ece2312aea99ea39b524b3e616f2030f49
Muthu Kumaran <muthu89kumaran@gmail.com>
2022-05-14 16:54:21 +0530

    Product migration file added

---
+++ database/migrations/2022_05_14_111326_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->string('id')->unique();
+            $table->string('product_id');
+            $table->float('price', 8, 2);
+            $table->float('tax', 8, 2);
+            $table->integer('available_stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
