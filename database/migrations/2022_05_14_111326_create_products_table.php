9be98c214f09531fdfb63838dcfb2d26410ada36
Muthu Kumaran <muthu89kumaran@gmail.com>
2022-05-14 17:50:22 +0530

    Product Name field added into product table

--- database/migrations/2022_05_14_111326_create_products_table.php
+++ database/migrations/2022_05_14_111326_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->string('id')->unique();
             $table->string('product_id');
+            $table->string('product_name');
             $table->float('price', 8, 2);
             $table->float('tax', 8, 2);
             $table->integer('available_stock');
